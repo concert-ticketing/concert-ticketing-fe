1545109898a31762f406bff5d1228b0d778f4a78
wogml3270 <wogml3270@naver.com>
2025-05-15 06:27:08 +0900

    [fix]: 불필요한 파일 제거, 캐시 초기화

--- src/types/imagetracer.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export interface ImageTracerType {
-  getImgdata: (canvas: HTMLCanvasElement) => any;
-  imagedataToSVG: (imgData: any, options?: Record<string, any>) => string;
-}
